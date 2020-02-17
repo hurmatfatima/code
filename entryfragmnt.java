6ff0d2456bdb688d91ef1dadb530f49c02f28b0f
Your Name <you@example.com>
2020-02-17 23:33:54 +0500

    first commit

---
+++ entryfragmnt.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.entry_fragmnt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class entryfragmnt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entryfragmnt);
+    }
+}
