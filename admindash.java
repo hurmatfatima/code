10d8263c18f07c95b33ba1b728e908002d0b4433
Muqadsa <61132926+Muqadsa@users.noreply.github.com>
2020-02-24 10:02:11 +0500

    Add files via upload

---
+++ admindash.java
@@ -0,0 +1,109 @@
+package com.example.myapplication.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bumptech.glide.request.RequestOptions;
+import com.example.myapplication.R;
+import com.glide.slider.library.Animations.DescriptionAnimation;
+import com.glide.slider.library.SliderLayout;
+import com.glide.slider.library.SliderTypes.BaseSliderView;
+import com.glide.slider.library.SliderTypes.TextSliderView;
+import com.glide.slider.library.Tricks.ViewPagerEx;
+
+import java.util.ArrayList;
+
+public class admindash extends AppCompatActivity implements BaseSliderView.OnSliderClickListener,
+        ViewPagerEx.OnPageChangeListener {
+    private SliderLayout mDemoSlider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admindash);
+        mDemoSlider = findViewById(R.id.slider);
+        ArrayList<Integer> listUrl = new ArrayList<>();
+        ArrayList<String> listName = new ArrayList<>();
+
+        listUrl.add(R.drawable.stdslide);
+        listName.add("registration -enroll new std");
+
+        listUrl.add(R.drawable.viewstdslide);
+        listName.add("histroy - view std's");
+
+        listUrl.add(R.drawable.entryslide);
+        listName.add("information - attendent info");
+
+        listUrl.add(R.drawable.reportslide);
+        listName.add("report -keep record");
+
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.centerCrop();
+        //.diskCacheStrategy(DiskCacheStrategy.NONE)
+        //.placeholder(R.drawable.placeholder)
+        //.error(R.drawable.placeholder);
+
+
+        for (int i = 0; i < listUrl.size(); i++) {
+            TextSliderView sliderView = new TextSliderView(this);
+            // if you want show image only / without description text use DefaultSliderView instead
+
+            // initialize SliderLayout
+            sliderView
+                    .image(listUrl.get(i))
+                    .description(listName.get(i))
+                    .setRequestOption(requestOptions)
+                    .setProgressBarVisible(true)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            sliderView.bundle(new Bundle());
+            sliderView.getBundle().putString("extra", listName.get(i));
+            mDemoSlider.addSlider(sliderView);
+        }
+
+        // set Slider Transition Animation
+        // mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Default);
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
+
+    }
+
+
+
+    @Override
+    public void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this, slider.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    }
+
+
